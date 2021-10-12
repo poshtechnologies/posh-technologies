0fb4988d36221658ad4078012e1a9e15bec6ca8f
praveen <praveen@gmail.com>
2021-10-12 14:33:58 +0000

    login.java is added

---
+++ login.java
@@ -0,0 +1,8 @@
+Public class Login
+{
+	public static void main()
+		{
+			System.out.println("Employee Details are printing: ");
+			System.out.println("Employee Information is printing: ");
+		}
+}
